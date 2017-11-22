6ca7cccc18d142ea80e8c024f70c1f3872a09d01
Literall <pnguyen@icgconsulting.com>
2017-11-22 00:29:51 -0700

    add more functionality

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost/blog_db');
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    created: {type: Date, default: Date.now},
+    body: String
+});
+
+let Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//     title: 'My Title',
+//     image: 'https://puu.sh/y3lN1/da6f0dc51e.png',
+//     body: 'this is my body from my post'
+// });
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.redirect("/blogs");
 })
 
-app.get("/new", (req, res) => {
-    res.render("new");
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.render("index", {blogs:blogs})
+        }
+    })
 })
 
-app.liste(3000, () => {
-    console.log("Listening on port 3000").
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
 })
